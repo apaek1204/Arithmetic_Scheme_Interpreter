31ad6f79d3e088686dec66e5c70d30836efc880b
Andrew D Paek <apaek1@student00.cse.nd.edu>
2016-09-13 18:30:59 -0400

    upload uscheme.cpp

--- uscheme.cpp
+++ uscheme.cpp
@@ -25,29 +25,122 @@ struct Node {
 
     friend ostream &operator<<(ostream &os, const Node &n);
 };
+Node::Node(string value, Node *left, Node *right){
+    this->value = value;
+    this->left = left;
+    this->right = right;
+}
 
+Node::~Node(){
+    delete left;
+    delete right;
+    left = nullptr;
+    right = nullptr;
+}
 ostream &operator<<(ostream &os, const Node &n) {
+    
+    os << "(";
+    if(n.left|| n.right){
+        os << "Node: value=" << n.value <<", left="<< *n.left << ", right="<< *n.right;
+    }
+    else{
+        os << "Node: value=" << n.value;
+    }
+    os << ")";
     return os;
 }
+
+
 
 // Parser ----------------------------------------------------------------------
 
 string parse_token(istream &s) {
+    char c1=s.peek();
     string token;
+    //skip whitespace
+    while(isspace(c1)){
+        c1=s.get();
+        c1 = s.peek();
+    }
+    //check next char
+    if(!isdigit(c1)){
+        token = s.get();
+    }
+    else{
+        token = s.get();
+        while(isdigit(s.peek())){
+            token += s.get();
+        }
+    }
     return token;
 }
 
 Node *parse_expression(istream &s) {
+    string token = parse_token(s);
+    Node *left=nullptr;
+    Node *right=nullptr;
+    if(token=="" || token==")"){
+        return nullptr;
+    }
+    if(token == "("){
+        token = parse_token(s);
+        left = parse_expression(s);
+        if(left){
+            right = parse_expression(s);
+        }
+        if(right){
+            parse_token(s);
+        }
+    }
     return new Node(token, left, right);
 }
 
 // Interpreter -----------------------------------------------------------------
 
-void evaluate_r(const Node *n, stack<int> &s) {
+int convert(string s){
+    int ans=0;
+    for(unsigned i=0; i<s.size(); i++){
+        ans = ans*10;
+        int temp = s[i]-'0';
+        ans = ans + temp;
+    }
+    return ans;
+}
+void evaluate_r(const Node *n, stack<int> &s) { 
+    if(n==nullptr){
+        return;
+    }
+    evaluate_r(n->right, s);
+    evaluate_r(n->left, s);
+
+    if(isdigit(n->value[0])){
+        s.push(convert(n->value));
+    }
+    else{
+        
+        int a= s.top();
+        s.pop();
+        int b = s.top();
+        s.pop();
+        if(n->value == "+"){
+            s.push(a+b);
+        }
+        else if(n->value == "-"){
+            s.push(a-b);
+        }
+        else if(n->value == "*"){
+            s.push(a*b);
+        }
+        else{
+            s.push(a/b);
+        }
+    }
 }
 
 int evaluate(const Node *n) {
-    return 0;
+    stack<int> s;
+    evaluate_r(n,s);
+    return s.top();
 }
 
 // Main execution --------------------------------------------------------------
